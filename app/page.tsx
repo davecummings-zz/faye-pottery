fef5315acd593431c7d088d68479b9b1e4a89835
Luna <luna@faye-pottery.local>
2026-03-06 06:26:08 -0600

    Remove newsletter signup from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import FeaturedProducts from '@/components/FeaturedProducts'
-import Newsletter from '@/components/Newsletter'
 
 export default function Home() {
   return (
@@ -9,7 +8,6 @@ export default function Home() {
       <Hero />
       <About />
       <FeaturedProducts />
-      <Newsletter />
     </>
   )
 }
